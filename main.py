bd2f73519ef172bda1628f7d1afb1422d11298d6
ivanetsas <ivanetsas@yandex-team.ru>
2023-09-02 11:47:30 +0300

    init our files

---
+++ main.py
@@ -0,0 +1,10 @@
+
+import sys
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print(sys.argv[1])
+    print(sys.argv[2])
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
